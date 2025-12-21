7b758d6e7deb597125a2cd3b5e3432ddf89579bf
Hema-devi <151372311+Hema-devi@users.noreply.github.com>
2025-12-21 20:34:26 +0530

    Smallest element

---
+++ 21.12.2025/Smallestelement.java
@@ -0,0 +1,13 @@
+class SmallestArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 5, 20, 3, 15};
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+        }
+
+        System.out.println("Smallest = " + min);
+    }
+}
