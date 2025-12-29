f3be63ee83c5b5c974b3472175e04fc2b5657d9a
Hema-devi <151372311+Hema-devi@users.noreply.github.com>
2025-12-29 20:25:07 +0530

    Armstrong

---
+++ 23.12.2025/Armstrong.java
@@ -0,0 +1,11 @@
+class Armstrong {
+    public static void main(String[] args) {
+        int n = 153, sum = 0, temp = n;
+        while (n > 0) {
+            int r = n % 10;
+            sum += r * r * r;
+            n /= 10;
+        }
+        System.out.println(sum == temp ? "Armstrong" : "Not Armstrong");
+    }
+}
