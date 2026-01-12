04c033d2ccd39a9aec05c3ea840a61ff9596ef69
Hema-devi <151372311+Hema-devi@users.noreply.github.com>
2026-01-12 20:46:20 +0530

    SumOfDigits

---
+++ 12.01.2026/SumOfDigits.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    static int sum(int n) {
+        if (n == 0)
+            return 0;
+        return (n % 10) + sum(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println("Sum of digits: " + sum(num));
+    }
+}
